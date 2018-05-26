9b554809d40241080d0cae2a6a455575522c4ddb
thiru <thiruppathi.muthukumar@ee.co.uk>
2018-05-26 01:14:22 +0100

    Delete API Keys. Regenerates new API Keys

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAWViajeKC3X24OORxBLCkjMWVOl2urTgA",
-  authDomain: "catch-the-fish-thiru.firebaseapp.com",
-  databaseURL: "https://catch-the-fish-thiru.firebaseio.com"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: ""
 });
 
 const base = Rebase.createClass(firebaseApp.database());
